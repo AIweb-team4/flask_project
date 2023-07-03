cbad1f42cc67334916f2e50c2e836933f0a1c6fe
JungwooJoon <69133633+JungwooJoon@users.noreply.github.com>
2023-07-03 11:05:10 +0900

    Update Crwaling.py

--- view/Crwaling.py
+++ view/Crwaling.py
@@ -6,9 +6,6 @@ from bs4 import BeautifulSoup
 from flask import Blueprint
 
 b_Cr = Blueprint('crwaling', __name__, url_prefix='/cr')
-
-api = 'https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=s%2B1EAl1Ky%2B8jsaOUZdvQMc7AwLUjHF2bYY4evN1MohRWAm48srSsvGlvxAjTGiKAevbHdccXmCFRYFzRbxO3cw%3D%3D'
-
 def DrugstoreJson(area1, area2):
     params = {'Q0': area1, 'Q1': area2 , 'QT':7}
     # Q1 : 지역 #QT : 날짜
